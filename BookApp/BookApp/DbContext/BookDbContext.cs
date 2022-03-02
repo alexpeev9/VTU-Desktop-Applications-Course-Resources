88b4734ac1e4e9c33df36b8bd0366fda7cb34eeb
alexpeev9 <alexpeev9@abv.bg>
2022-03-02 09:28:10 +0200

    Add BookProject

---
+++ BookApp/BookApp/DbContext/BookDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace BookApp.DbContext
+{
+    using BookApp.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class BookDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if(optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=BookDatabase;Integrated security=true;");
+            }
+        }
+    }
+}
---
+++ BookApp/BookApp/Register.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace BookApp
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RegisterButton.Location = new System.Drawing.Point(312, 339);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(166, 55);
+            this.RegisterButton.TabIndex = 1;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RegisterButton);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RegisterButton;
+    }
+}
