255c9b7884bb6cfc278898e6de7d0b60bce91ea8
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2022-04-20 09:16:34 +0300

    Add Web Project

---
+++ MovieApp/MovieApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<Movie> Movies { get;  set; }
+	}
+}
---
+++ MovieApp/MovieApp/Data/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data.Models
+{
+	public class Movie
+	{
+		[Key]
+		public Guid Id { get; set; }
+		public string Titile { get; set; }
+		public string ImageUrl { get; set; }
+		public int ReleaseYear { get; set; }
+	}
+}
