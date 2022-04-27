f88c72697ecc4956c66eb87d2ec26597e3076005
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2022-04-27 09:20:28 +0300

    Add Song Web Project

---
+++ SongWebApp/SongWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SongWebApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SongWebApp.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Song> Songs { get; set; }
+	}
+}
